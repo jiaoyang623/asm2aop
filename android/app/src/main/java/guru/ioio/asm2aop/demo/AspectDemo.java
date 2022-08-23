51284db88c630cbeaa88b68e2d373b88ae67d693
jiaoyang <jiaoyang@360.cn>
2022-08-23 22:18:10 +0800

    修改APT生成代码逻辑

--- android/app/src/main/java/guru/ioio/asm2aop/demo/AspectDemo.java
+++ android/app/src/main/java/guru/ioio/asm2aop/demo/AspectDemo.java
@@ -5,12 +5,12 @@ import guru.ioio.asm2aop_annotation.Before;
 
 @Asm2Aop
 public class AspectDemo {
-    @Before("execution android.app.Activity.onCreate(..)")
+    @Before("execution void android.app.Activity.onCreate(..)")
     public void beforeOnCreate() {
         System.out.println("asm2aop: Activity.onCreated");
     }
 
-    @Before("execution android.app.Activity.onDestroy(..)")
+    @Before("execution void android.app.Activity.onDestroy(..)")
     public void beforeOnDestroy() {
         System.out.println("asm2aop: Activity.onDestroy");
     }
